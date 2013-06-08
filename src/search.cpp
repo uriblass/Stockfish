b852c7d464972685f8c02cf312e90b2337d388fa
Uri Blass <uriblass@gmail.com>
2013-06-08 13:19:09 +0300

    different formula for less LMR reduction for counter moves

--- src/search.cpp
+++ src/search.cpp
@@ -949,10 +949,8 @@ split_point_start: // At split points actual search starts from here
           &&  move != ss->killers[0]
           &&  move != ss->killers[1])
       {
-          ss->reduction = reduction<PvNode>(depth, moveCount);
-          if (move == countermoves[0] || move == countermoves[1])
-              ss->reduction = std::max(DEPTH_ZERO, ss->reduction-ONE_PLY);
-
+		  ss->reduction = reduction<PvNode>(depth, moveCount-
+			  (moveCount>1)* (move == countermoves[0] || move == countermoves[1]));
           Depth d = std::max(newDepth - ss->reduction, ONE_PLY);
           if (SpNode)
               alpha = splitPoint->alpha;
