7fe1ac413d089ebff75e4cd008140d8592ae8f23
Uri Blass <uriblass@gmail.com>
2013-06-16 04:01:36 +0300

    reduce less only first counter move and not second counter move

--- src/search.cpp
+++ src/search.cpp
@@ -952,7 +952,7 @@ split_point_start: // At split points actual search starts from here
           if (!PvNode && cutNode)
               ss->reduction += ONE_PLY;
 
-          if (move == countermoves[0] || move == countermoves[1])
+          if (move == countermoves[0])
               ss->reduction = std::max(DEPTH_ZERO, ss->reduction-ONE_PLY);
 
           Depth d = std::max(newDepth - ss->reduction, ONE_PLY);
