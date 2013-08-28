771249f035fcd3005e591b402e44af8e765dcd0c
Uri Blass <uriblass@gmail.com>
2013-08-28 06:17:51 +0300

    adding condition that is about not changing your mind in the last iteration to easy move

--- src/search.cpp
+++ src/search.cpp
@@ -451,6 +451,7 @@ namespace {
                 && !stop
                 &&  PVSize == 1
                 &&  bestValue > VALUE_MATED_IN_MAX_PLY
+				&& BestMoveChanges<0.1
                 && (   RootMoves.size() == 1
                     || Time::now() - SearchTime > (TimeMgr.available_time() * 20) / 100))
             {
