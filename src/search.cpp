8ed2d18ca78529514218bfc01edfefd9c57e71db
Uri Blass <uriblass@gmail.com>
2013-09-09 06:36:52 +0300

    another try to do less reduction that combine my best try that failed stage 1 with more than 50% with more extensions when the side that does not give check has the advantage

--- src/search.cpp
+++ src/search.cpp
@@ -835,7 +835,11 @@ moves_loop: // When in check and at SpNode search starts from here
           ext = ONE_PLY;
 
       else if (givesCheck && pos.see_sign(move) >= 0)
+	  {
           ext = ONE_PLY / 2;
+		  if (ss->staticEval<Value(-200)||inCheck)
+			  ext=ONE_PLY;
+	  }
 
       // Singular extension search. If all moves but one fail low on a search of
       // (alpha-s, beta-s), and just one fails high on (alpha, beta), then that move
@@ -942,7 +946,8 @@ moves_loop: // When in check and at SpNode search starts from here
           && !captureOrPromotion
           &&  move != ttMove
           &&  move != ss->killers[0]
-          &&  move != ss->killers[1])
+          &&  move != ss->killers[1]
+		  && ((!givesCheck)||(ext ==0)||(ss-1)->staticEval!=VALUE_NONE))
       {
           ss->reduction = reduction<PvNode>(improving, depth, moveCount);
 
