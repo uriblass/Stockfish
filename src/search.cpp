67bd51fb1d72873ed16130760e0d7ad888721a88
Uri Blass <uriblass@gmail.com>
2014-01-06 01:22:12 +0200

    try to use the branching factor in the time management that mean playing faster if you use a lot of time for the first move that in part of the cases is because of hash tables

--- src/search.cpp
+++ src/search.cpp
@@ -117,7 +117,7 @@ namespace {
 
 
 /// Search::init() is called during startup to initialize various lookup tables
-
+Time::point end_iteration_time;
 void Search::init() {
 
   int d;  // depth (ONE_PLY == 2)
@@ -416,7 +416,7 @@ namespace {
             && bestValue >= VALUE_MATE_IN_MAX_PLY
             && VALUE_MATE - bestValue <= 2 * Limits.mate)
             Signals.stop = true;
-
+		end_iteration_time=Time::now() - SearchTime;
         // Do we have time for the next iteration? Can we stop searching now?
         if (Limits.use_time_management() && !Signals.stop && !Signals.stopOnPonderhit)
         {
@@ -429,7 +429,7 @@ namespace {
             // Stop the search if most of the available time has been used. We
             // probably don't have enough time to search the first move at the
             // next iteration anyway.
-            if (Time::now() - SearchTime > (TimeMgr.available_time() * 62) / 100)
+            if (end_iteration_time > (TimeMgr.available_time() * 62) / 100)
                 stop = true;
 
             // Stop the search early if one move seems to be much better than others
@@ -1627,9 +1627,10 @@ void check_time() {
   }
 
   Time::point elapsed = Time::now() - SearchTime;
+  double factor=(elapsed+1.0)/(end_iteration_time+1.0);
   bool stillAtFirstMove =    Signals.firstRootMove
                          && !Signals.failedLowAtRoot
-                         &&  elapsed > TimeMgr.available_time();
+                         &&  (elapsed > TimeMgr.available_time()/factor);
 
   bool noMoreTime =   elapsed > TimeMgr.maximum_time() - 2 * TimerThread::Resolution
                    || stillAtFirstMove;
