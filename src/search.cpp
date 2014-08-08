c32aa6edb7ee4935ccd942fb6fa97ebe3601154c
Uri Blass <uriblass@gmail.com>
2014-08-08 03:56:16 +0300

    verifying that the source of error is the relevant line in the comment

--- src/search.cpp
+++ src/search.cpp
@@ -468,7 +468,7 @@ namespace {
     ss->ply = (ss-1)->ply + 1;
     (ss+1)->skipNullMove = false; (ss+1)->reduction = DEPTH_ZERO;
     (ss+2)->killers[0] = (ss+2)->killers[1] = MOVE_NONE;
-	Depth reduction_so_far=iteration*ONE_PLY;
+//this is the source of error	Depth reduction_so_far=iteration*ONE_PLY;
 //	Depth reduction_so_far=(iteration+1-ss->ply)*ONE_PLY;
 //	reduction_so_far=reduction_so_far-depth;
     // Used to send selDepth info to GUI
@@ -604,8 +604,8 @@ namespace {
                  + (abs(beta) < VALUE_KNOWN_WIN ? int(eval - beta) / PawnValueMg * ONE_PLY
                                                 : DEPTH_ZERO);
 	//	R=R-(reduction_so_far/4);
-		if (R<3*ONE_PLY)
-			R=3*ONE_PLY;
+	//	if (R<3*ONE_PLY)
+	//		R=3*ONE_PLY;
         pos.do_null_move(st);
         (ss+1)->skipNullMove = true;
         nullValue = depth-R < ONE_PLY ? -qsearch<NonPV, false>(pos, ss+1, -beta, -beta+1, DEPTH_ZERO)
